a7369752bd02059cc08e013ad23334101ac8e462
Minjune <minjune8506@gmail.com>
2022-02-08 21:00:22 +0900

    5568 : 카드놓기 미완성 풀이

---
+++ SKHU/5568.cpp
@@ -0,0 +1,91 @@
+/*
+ * 5568 : 카드놓기
+ *
+ * 카드 n(4 <= n <= 10)장을 바닥에 나란히 놓고 놀고있다.
+ * 각 카드에는 1이상 99이하의 정수가 적혀있다.
+ * 이 카드 중에서 k(2 <= k <=4)장을 선택하고, 가로로 나란히 정수를 만들기로 했다.
+ * 상근이가 만들 수 있는 정수는 모두 몇가지일까?
+ * 
+ * ex) 카드 5장 (1, 2, 3, 13, 21)
+ * 3장을 선택해서 정수를 만드려고 한다.
+ * 2, 1, 13 -> 2113 을 만들 수 있다.
+ * 21, 1, 3 -> 2113을 만들 수 있다. 이렇게 한 정수를 만드는 조합이 여러가지 일 수 있다.
+ * 
+ * n장의 카드에 적힌 숫자가 주어졌을 때, 그 중 k개를 선택해서 만들 수 있는 정수의 개수를 구하는 프로그램
+ * 
+ * 입력
+ * 1 : n
+ * 2 : k
+ * 3 ~ : 카드에 적혀 있는 수
+ * 
+ * 출력
+ * 만들 수 있는 정수의 개수
+*/
+
+/*
+ * 풀이
+ * kCn x
+ * 중복인 숫자는 어떻게 처리해야 할지?
+ * 
+ * ex) 1 2 12 1
+ * 4 C 2 ->
+ * 4!
+ * 2! * (4 - 2)! => 6
+ * 
+ * 4 P 2 ->
+ * 4!
+ * (4 - 2)! -> 12
+ */
+#include <iostream>
+#include <algorithm>
+#include <set>
+
+using namespace std;
+
+int n;
+int k;
+
+int permutation(int *cards)
+{
+	int num = n;
+	int ret = 1;
+
+	for (int i = 0 ; i < k ; i++)
+	{
+		ret *= num;
+		num--;	
+	}
+	return (ret);
+}
+
+void solve(int *cards, int *ary)
+{
+	set<int> s;
+	int num;
+	
+	//..?
+}
+
+int main(void)
+{
+	int *cards;
+	int *ary;
+	int per;
+
+	// input
+	cin >> n >> k;
+	cards = new int[n];
+	for (int i = 0 ; i < n ; i++)
+		cin >> cards[i];
+
+	per = permutation(cards);
+	
+	ary = new int[per];
+	solve(cards, ary);
+	cout << "permutation : " << per << "\n";
+
+	// check input
+	for (int i = 0 ; i < n ; i++)
+		cout << cards[i] << " ";
+	cout << "\n";
+}
