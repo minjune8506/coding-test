0d97ef448df6faeb459373ce1a30ce4486f70e88
Minjune <minjune8506@gmail.com>
2022-04-04 14:40:50 +0900

    1427 : 소트인사이드 (Sort)

---
+++ BOJ/12.정렬/1427.cpp
@@ -0,0 +1,28 @@
+/**
+ * 1427 : 소트인사이드
+ * 
+ * 배열을 정렬하는 쉽다. 수가 주어지면, 그 수의 각 자리수를 내림차순으로 정렬해보자.
+ * 
+ * 입력
+ * 첫째 줄에 정렬하려고 하는 수 N이 주어진다.
+ * N은 1,000,000,000보다 작거나 같은 자연수이다.
+ * 
+ * 출력
+ * 첫째 줄에 자릿수를 내림차순으로 정렬한 수를 출력한다.
+ */
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main(void)
+{
+	string N;
+
+	cin >> N;
+	sort(N.begin(), N.end(), greater<char>());
+	for (int i = 0 ; i < N.size() ; i++)
+		cout << N[i];
+	cout << "\n";
+}
