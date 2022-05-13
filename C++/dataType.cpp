5a1082ea5a952bd0471345281a22ebdafadb9d52
Minjune <minjune8506@gmail.com>
2022-05-13 23:19:59 +0900

    C++ Data Type Test Code

---
+++ C++/dataType.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	cout << "bool : " << sizeof(bool) << endl;
+	cout << "short : " << sizeof(short) << endl;
+	cout << "unsigned short : " << sizeof(unsigned short) << endl;
+	cout << "int : " << sizeof(int) << endl;
+	cout << "unsigned int : " << sizeof(unsigned int) << endl;
+	cout << "long : " << sizeof(long) << endl;
+	cout << "unsigned long : " << sizeof(unsigned long) << endl;
+	cout << "long long : " << sizeof(long long) << endl;
+	cout << "unsigned long long : " << sizeof(unsigned long long) << endl;
+	cout << "int : " << sizeof(int) << endl;
+	cout << "__int8_t : " << sizeof(__int8_t) << endl;
+	cout << "__int16t : " << sizeof(__int16_t) << endl;
+	cout << "__int32t : " << sizeof(__int32_t) << endl;
+	cout << "__int64t : " << sizeof(__int64_t) << endl;
+
+}
