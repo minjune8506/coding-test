cabbe5cb240fd866007c28510f51d40d9efc0216
Minjune <minjune8506@gmail.com>
2022-05-31 13:57:54 +0900

    Programmers : Level2

---
+++ Programmers/Level2/더 맵게.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int solution(vector<int> scoville, int K) {
+    int answer = 0;
+    priority_queue<int, vector<int>, greater<int> > pq;
+    
+    for (int i = 0 ; i < scoville.size() ; i++) {
+        pq.push(scoville[i]);
+    }
+    while (pq.size() >= 2 && pq.top() < K) {
+        int first = pq.top();
+        pq.pop();
+        int second = pq.top();
+        pq.pop();
+        pq.push(first + second * 2);
+        answer++;
+    }
+    if (pq.top() < K)
+        answer = -1;
+    return answer;
+}
