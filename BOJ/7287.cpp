3c348dba430cd94f8444e85614a3d49967c3cba4
Minjune <minjune8506@gmail.com>
2023-01-16 11:26:11 +0900

    7287 (등록)

---
+++ BOJ/7287.cpp
@@ -0,0 +1,13 @@
+/**
+ * @file 7287.cpp
+ * @author Minjune (minjune8506@gmail.com)
+ * @brief 등록
+ * @date 2023-01-16
+ * 
+ * 자신이 BOJ에서 맞은 문제의 수와 아이디를 출력하는 프로그램을 작성
+ */
+#include <iostream>
+
+int main(void) {
+	std::cout << "161" << '\n' << "minjune8506" << std::endl;
+}
